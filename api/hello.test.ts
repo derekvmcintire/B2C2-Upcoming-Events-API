c75f602bdcd75cd51c1bc877f2491bd27d5f310d
Derek McIntire <77245762+derekvmcintire@users.noreply.github.com>
2025-01-20 15:51:46 -0500

    Set up unit testing and write first test for /hello (#6)
    * Add test dependencies
    
    * First passing test
    
    * Pretty

---
+++ api/hello.test.ts
@@ -0,0 +1,60 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import hello from './hello';
+
+describe('hello API', () => {
+  it('should respond with "Hello World!" when no name is provided', () => {
+    const req = { query: {} } as Partial<VercelRequest> as VercelRequest;
+
+    const setHeaderMock = jest.fn(); // Mock setHeader
+    const jsonMock = jest.fn();
+    const res = {
+      setHeader: setHeaderMock,
+      json: jsonMock,
+      status: jest.fn().mockReturnThis(),
+      end: jest.fn(),
+    } as unknown as VercelResponse;
+
+    hello(req, res);
+
+    expect(setHeaderMock).toHaveBeenCalledWith('Access-Control-Allow-Origin', '*');
+    expect(setHeaderMock).toHaveBeenCalledWith(
+      'Access-Control-Allow-Methods',
+      'GET, POST, OPTIONS'
+    );
+    expect(setHeaderMock).toHaveBeenCalledWith(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization'
+    );
+    expect(jsonMock).toHaveBeenCalledWith({
+      message: 'Hello World!',
+    });
+  });
+
+  it('should respond with "Hello [name]!" when a name is provided', () => {
+    const req = { query: { name: 'Alice' } } as Partial<VercelRequest> as VercelRequest;
+
+    const setHeaderMock = jest.fn();
+    const jsonMock = jest.fn();
+    const res = {
+      setHeader: setHeaderMock,
+      json: jsonMock,
+      status: jest.fn().mockReturnThis(),
+      end: jest.fn(),
+    } as unknown as VercelResponse;
+
+    hello(req, res);
+
+    expect(setHeaderMock).toHaveBeenCalledWith('Access-Control-Allow-Origin', '*');
+    expect(setHeaderMock).toHaveBeenCalledWith(
+      'Access-Control-Allow-Methods',
+      'GET, POST, OPTIONS'
+    );
+    expect(setHeaderMock).toHaveBeenCalledWith(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization'
+    );
+    expect(jsonMock).toHaveBeenCalledWith({
+      message: 'Hello Alice!',
+    });
+  });
+});
