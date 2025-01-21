fa94d4e7c06c6d7d7dd82b46bf7faad975684a60
derekvmcintire <derekcommonground@gmail.com>
2025-01-21 14:25:52 -0500

    Remove passing header in test

--- __tests__/smoke/smoke-tests.test.ts
+++ __tests__/smoke/smoke-tests.test.ts
@@ -6,10 +6,8 @@ if (process.env.CI !== 'true') {
 }
 
 const API_BASE_URL = process.env.API_URL;
-const VERCEL_TOKEN = process.env.VERCEL_TOKEN;
 
 console.log("API_BASE_URL in the smoke test file is: ", API_BASE_URL);
-console.log("VERCEL_TOKEN in the smoke test file iS: ", VERCEL_TOKEN);
 
 if (!API_BASE_URL) {
   throw new Error('API_URL is not defined in CI or local environment');
@@ -18,11 +16,7 @@ if (!API_BASE_URL) {
 describe('Smoke Tests', () => {
   it('should perform a smoke test', async () => {
     console.log("Making request to hello endpoint...");
-    const response = await fetch(`${API_BASE_URL}/api/hello`, {
-      headers: {
-        Authorization: `Bearer ${VERCEL_TOKEN}`,
-      },
-    });
+    const response = await fetch(`${API_BASE_URL}/api/hello`);
     expect(response.status).toBe(200);
   });
 });
